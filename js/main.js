d5a7361a4b835cdd135c0907d4eabca1afc21c48
ingrommell <ingrommell@gmail.com>
2023-08-08 16:53:25 -0400

    Trabajando ya con javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 window.alert ("Bienvenido a la página de Rommell Delgado");
+
+const menuItems = document.querySelectorAll (".barra_navegacion") 
+
+console.log (menuItems);
+
+menuItems.forEach(function (item) {
+    item.addEventListener("click",function (e) {
+        const currentItem = document.querySelector(".elemento_navegacion")
+        currentItem.classList.remove("elemento_navegacion")
+        e.target.classList.add("activate")
+    })
+})
