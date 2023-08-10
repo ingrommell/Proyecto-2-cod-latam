90a10fe5c73a0bbc3a066a617e58c15158c8b1ab
ingrommell <ingrommell@gmail.com>
2023-08-09 20:45:47 -0400

    Arreglando el logo

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-window.alert ("Bienvenido a la página de Rommell Delgado");
+window.alert("Bienvenido a la página de Rommell Delgado");
 
-const menuItems = document.querySelectorAll (".barra_navegacion");
+const menuItems = document.querySelectorAll(".barra_navegacion");
 
 const menuBtn = document.querySelector("#menu-btn");
 
-menuBtn.addEventListener("click",function(){
-    document.body.classList.toggle("mobile-menu-activate")
-})
+menuBtn.addEventListener("click", function () {
+  document.body.classList.toggle("mobile-menu-activate");
+});
 
 menuItems.forEach(function (item) {
-    item.addEventListener("click",function (e) {
-        const currentItem = document.querySelector(".elemento_navegacion")
-        currentItem.classList.remove("elemento_navegacion")
-        e.target.classList.add("activate")
-    })
-})
+  item.addEventListener("click", function (e) {
+    const currentItem = document.querySelector(".elemento_navegacion");
+    currentItem.classList.remove("elemento_navegacion");
+    e.target.classList.add("activate");
+  });
+});
